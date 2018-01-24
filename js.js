68e7ed23a1e822e267abe655492cedf6cfe26038
Nicole jeffries <nicolejeffries0118@gmail.com>
2018-01-24 17:08:06 -0500

    First API push

---
+++ js.js
@@ -0,0 +1,43 @@
+$(() => {
+
+
+    $('#coinInfo').click(() => {  
+        $.ajax({
+            method: 'GET',
+            url: "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP,ZRX,TNT,GVT,PPT,LTC&tsyms=USD"
+        }).done((res) => { 
+       
+         console.log(res)
+        let btc = res.BTC.USD
+        let ltc = res.LTC.USD
+        let xrp = res.XRP.USD
+
+        let zrx = res.ZRX.USD  
+        let tnt = res.TNT.USD 
+        let gvt = res.GVT.USD 
+        let ppt = res.PPT.USD   
+
+        console.log(btc)
+        console.log(ltc)
+        let btcContainer=(`<td>${btc}</td>`)
+        $('#btcContainer').append(btcContainer)
+        let ltcContainer=(`<td>${ltc}</td>`)
+        $('#ltcContainer').append(ltcContainer)
+        let  xrpContainer=(`<td>${xrp}</td>`)  
+        $('#xrpContainer').append(xrpContainer)
+        let zrxContainer=(`<td>${zrx}</td>`)
+        $('#zrxContainer').append(zrxContainer)
+        let tntContainer=(`<td>${tnt}</td>`)
+        $('#tntContainer').append(tntContainer)
+
+        let gvtContainer=(`<td>${gvt}</td>`)  
+        $('#gvtContainer').append(gvtContainer)
+        let pptContainer=(`<td>${ppt}</td>`)
+        $('#pptContainer').append(pptContainer)
+        })
+
+        })
+
+    })
+
+
